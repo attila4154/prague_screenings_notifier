7d641b5e6bd02d273997af5f05604f99dc7c4c25
Danila Makulov <attila415422@gmail.com>
2026-03-14 22:54:24 +0100

    filter screenings by cinema names

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,17 @@ async function main() {
   console.log(screenings);
   console.log(logs);
   console.log(`parsing took ${time}mls`);
+
+  const filtered = screenings
+    .filter((s) => !s.cinema.startsWith("CineStar"))
+    .filter(
+      (s) =>
+        !(s.cinema.startsWith("Cinema City") && !s.cinema.includes("Flora")),
+    );
+
+  console.log(screenings.length);
+  console.log(filtered.length);
+  console.log(new Set(filtered.map((s) => s.cinema)));
 }
 
 main();
